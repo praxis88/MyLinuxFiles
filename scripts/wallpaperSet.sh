93572ed5bedafb441fd12ea6754c64d0435c8206
Christopher Hall <hall.christopher88@gmail.com>
2020-04-22 10:50:36 -0500

    Menu Tweaking

--- scripts/wallpaperSet.sh
+++ scripts/wallpaperSet.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
-pape=$(ls /usr/share/pixmaps/papes/ | rofi -dmenu -p "Which Pape? >  "); echo "${pape}"
+pape=$(ls /usr/share/pixmaps/papes/ | rofi -dmenu -p "Which Pape? >  ")
 xwallpaper --screen 0 --stretch /usr/share/pixmaps/papes/$pape 
 dunstify "Wallpaper Set" "$pape"
