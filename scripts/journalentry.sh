4a07712c20aee6d2bc09490d980fe934355ec3a5
Christopher Hall <hall.christopher88@gmail.com>
2020-04-26 16:09:04 -0500

    Fixed open bug

--- scripts/journalentry.sh
+++ scripts/journalentry.sh
@@ -2,5 +2,5 @@
 
 now=$(date +%D | sed 's:/:-:g')
 touch /home/christopher/Documents/Journaling/$now
-i3-sensible-terminal -e 'nvim /home/christopher/Documents/Journaling/$now'
+i3-sensible-terminal -e 'nvim /home/christopher/Documents/Journaling/'$now
 
