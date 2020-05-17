b59aa4b6f1b3aee3de0b0fe3fea0f4ede59ab682
Christopher Hall <hall.christopher88@gmail.com>
2020-05-17 14:40:51 -0500

    backup stuff

--- scripts/journalentry.sh
+++ scripts/journalentry.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 now=$(date +%D | sed 's:/:-:g')
-touch /home/christopher/Documents/Journaling/$now
+#touch /home/christopher/Documents/Journaling/$now
 i3-sensible-terminal -e 'nvim /home/christopher/Documents/Journaling/'$now
 
