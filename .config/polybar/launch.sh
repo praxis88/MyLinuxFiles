60c93701c8d72cb7b3dc8b6bfeb2052996235d7f
Christopher Hall <hall.christopher88@gmail.com>
2021-02-06 07:31:32 -0600

    desktop sync

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,6 +6,16 @@ killall -q polybar
 #launch bar
 
 echo "---" | tee -a /tmp/polybar1.log 
-polybar bar1 >> /tmp/polybar1.log 2>&1 &
+
+
+
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload bar1 &
+  done
+else
+  polybar --reload bar1 &
+fi
 
 echo "Bar Launched..."
--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,11 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-#kill all running instances
-killall -q polybar
-
-#launch bar
-
-echo "---" | tee -a /tmp/polybar1.log 
-polybar bar1 >> /tmp/polybar1.log 2>&1 &
-
-dunstify "PolyBar Relaunched" "by launch.sh"
+exec /home/christopher/.config/polybar/launch.sh
