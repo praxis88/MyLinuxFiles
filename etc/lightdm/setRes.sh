53b24985d32bbcd6496db0a341058c96f11d9b8a
Christopher Hall <hall.christopher88@gmail.com>
2021-05-29 21:15:04 -0500

    updating and removing junk

---
+++ etc/lightdm/setRes.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+xrandr --output DP-0 --off
+xrandr --output DVI-D-0 --off
+xrandr --output HDMI-0 --mode 1920x1080
+xrandr --output HDMI-1 --off
--- scripts/wallpaperSet.sh
+++ scripts/wallpaperSet.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 
-pape=$(ls /usr/share/pixmaps/papes/ | rofi -dmenu -p "Which Pape? >  ")
-xwallpaper --screen 0 --stretch /usr/share/pixmaps/papes/$pape 
+pape=$(ls /usr/share/backgrounds/ | rofi -dmenu -p "Which Pape? >  ")
+papepath=/usr/share/backgrounds/$pape
+xwallpaper --screen 0 --stretch $papepath
+sed -i "s|background =.*|background = $papepath|g" /etc/lightdm/slick-greeter.conf
 dunstify "Wallpaper Set" "$pape"
